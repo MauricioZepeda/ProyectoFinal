c530b458df11bc6422febfa5dd2a49303605e8f2
Mauricio Zepeda <mauro@MacBook-Pro-de-Mauricio.local>
2020-07-19 04:12:45 -0400

    api weather

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,8 @@
-const base_url = 'http://localhost:1337';
-const entidad = 'usuario'
+const api_key_weather = 'c272d7c335cc451f9e90d12485a42f0b';
+const weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=';
 
-export const urlUsuario = (id='') => `${ base_url }/${ entidad }/${id}`;   
+const base_url = 'http://localhost:1337';
+const usuario = 'usuario';
+
+export const urlUsuario = (id='') => `${ base_url }/${ usuario }/${id}`;   
+export const urlWeather = (ciudad = 'london') => `${ weather_url }=${ciudad}&appid=${api_key_weather}`;  
